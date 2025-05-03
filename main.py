30f8f504bdfe73e1b8f98472d74f9cf5158646e4
fav <andviic@gmail.com>
2025-05-03 16:08:31 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,38 @@
+password = input('Введите пароль: ')
+
+
+def is_very_long(password):
+    return len(password) >= 12
+
+
+def has_difgit(password):
+    return any(digit.isdigit() for digit in password)
+
+
+def has_lower_letters(password):
+    return any(lower_letter.islower() for lower_letter in password)
+
+
+def has_upper_letters(password):
+    return any(upper_letter.isupper() for upper_letter in password)
+
+
+def has_symbols(password):
+    return any(not symbol.isalpha() and not symbol.isdigit()
+               for symbol in password)
+
+
+functions = [
+    is_very_long,
+    has_difgit,
+    has_lower_letters,
+    has_upper_letters,
+    has_symbols
+]
+
+score = 0
+for password_strength in functions:
+    if password_strength(password):
+        score += 2
+
+print(f"Рейтинг пароля: {score}")
